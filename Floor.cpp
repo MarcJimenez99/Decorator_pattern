417834e7bbb656101ab0b4d47f66c8502dad3aad
Neha Gupta <neha45556@gmail.com>
2019-02-12 23:05:41 -0800

    fixed floor

--- Floor.cpp
+++ Floor.cpp
@@ -1,12 +1,12 @@
 #include "Floor.h"
-
+#include <math.h>
 Floor :: Floor(){}
 
 double Floor :: Floor2(Base * num ){	
-	if(num % 1 == 0){
+	if(fmod(num, 1)  == 0){
 		return num;
 	}
-	double remain = num % 1;
+	double remain = fmod(num, 1);
 	num = num - remain;
 	return num;
 }
