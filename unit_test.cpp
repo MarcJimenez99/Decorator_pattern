b1dd00acf3c975e0a4c8f9c59f993e999c6bf3a9
Neha Gupta <neha45556@gmail.com>
2019-02-14 13:26:36 -0800

    add test cases

--- unit_test.cpp
+++ unit_test.cpp
@@ -11,292 +11,162 @@
 #include "VectorContainer.h"
 #include "bubble.h"
 #include "SelectionSort.h"
-//#include "base.hpp"
-//#include "sort.hpp"
-//#include "base.h"
-//class Bubble;
-//class SelectionSort;
-//class VectorContainer;
-//class ListContainer;
+#include "Ceil.cpp"
+#include "Floor.cpp"
+#include "Abs.cpp"
+#include "Decorator.hpp"
+#include "Trunc.cpp"
+#include "Paren.cpp"
 
-TEST(StrategyTest, PopulateList) {
+TEST(CeilTest, wholeNum){
+	Op * five = new Op(5.0);
+	Op * three = new Op(3.0);
+	Mult* TreeA = new Mult(five, three);
+	
+	Ceil* ceil2 = new Ceil(TreeA);
+	ASSERT_EQ(ceil2->evaluate(), 15.0);
+}
 
-	Op* seven = new Op(7);
-	Op* four = new Op(4);
-	Mult* TreeA = new Mult(seven, four);
-
+TEST(CeilTest, num){
+	Op* four = new Op(4.2);
 	Op* three = new Op(3);
-	Op* two = new Op(2);
-	Add* TreeB = new Add(three, two);
-
-	Op* ten = new Op(10);
-	Op* six = new Op(6);
-	Sub* TreeC = new Sub(ten, six);
-
-	ListContainer* container = new ListContainer();
-	container->add_element(TreeA);
-	container->add_element(TreeB);
-	container->add_element(TreeC);
-
-	ASSERT_EQ(container->size(), 3);
-	EXPECT_EQ(container->at(0)->evaluate(), 28);
-	EXPECT_EQ(container->at(1)->evaluate(), 5);
-	EXPECT_EQ(container->at(2)->evaluate(), 4);
-
-}
-TEST(StrategyTest, BubbleList) {
+	Add* TreeA = new Add(four, three);
 	
-	Op* seven = new Op(7);
-        Op* four = new Op(4);
-        Mult* TreeA = new Mult(seven, four);
-
-        Op* three = new Op(3);
-        Op* two = new Op(2);
-        Add* TreeB = new Add(three, two);
-
-        Op* ten = new Op(10);
-        Op* six = new Op(6);
-        Sub* TreeC = new Sub(ten, six);
-
-        ListContainer* container = new ListContainer();
-        container->add_element(TreeA);
-        container->add_element(TreeB);
-        container->add_element(TreeC);
-
-	container->set_sort_function(new Bubble());
-	container->sort();
-
-	ASSERT_EQ(container->size(), 3);
-        EXPECT_EQ(container->at(0)->evaluate(), 4);
-	EXPECT_EQ(container->at(1)->evaluate(), 5);
-	EXPECT_EQ(container->at(2)->evaluate(), 28);
-
-}
-TEST(StrategyTest, EmptyList) {
-	ListContainer* container = new ListContainer();
-	ASSERT_EQ(container->size(), 0);
-	EXPECT_EQ(container->at(0), nullptr); 
-}
-TEST(StrategyTest, NegativeBubbleList) {
 	Op* two = new Op(2);
-	Op* four = new Op(4);
+	Mult* TreeB = new Mult(two, TreeA);
+	
+	Ceil* ceil2 = new Ceil(TreeB);
+	ASSERT_EQ(ceil2->evaluate(), 15.0);
+}
+
+TEST(AbsTest, negNum){
+	Op * two = new Op(-2.0);
+	Op * four = new Op(4.0);
 	Mult* TreeA = new Mult(two, four);
-
-	Op* five = new Op(5);
-	Op* one = new Op(1);
-	Sub* TreeB = new Sub(five, one);
 	
-	Op* seven = new Op(-7);
-	Op* three = new Op(-3);
-	Add* TreeC = new Add(seven, three);
+	Abs* ab2 = new Abs(TreeA);
+	ASSERT_EQ(ab2->evaluate(), 8.0);
+}
 
-	ListContainer* container = new ListContainer();
-
-	container->add_element(TreeA);
-        container->add_element(TreeB);
-        container->add_element(TreeC);
+TEST(AbsTest, posNum){
+	Op* eight = new Op(8.0);
+	Op* seven = new Op(7.0);
+	Mult* TreeA = new Mult(eight, seven);
 	
-	container->set_sort_function(new Bubble());
-	container->sort();
+	Abs* ab = new Abs(TreeA);
+	ASSERT_EQ(ab->evaluate(), 56.0);
+}
+
+TEST(FloorTest, num){
+	Op* one = new Op(1.0);
+	Op* two = new Op(2.5);
+	Add* TreeA = new Add(one,two);
 	
-	ASSERT_EQ(container->size(), 3);
-	EXPECT_EQ(container->at(0)->evaluate(), -10);
-	EXPECT_EQ(container->at(1)->evaluate(), 4);
-	EXPECT_EQ(container->at(2)->evaluate(), 8); 
+	Floor * floor2 = new Floor(TreeA);
+	ASSERT_EQ(floor2->evaluate(), 3.0);
 }
-TEST(StrategyTest, SelectionSortList) {
 
-	Op* seven = new Op(7);
-        Op* four = new Op(4);
-        Mult* TreeA = new Mult(seven, four);
-
-        Op* three = new Op(3);
-        Op* two = new Op(2);
-        Add* TreeB = new Add(three, two);
-
-        Op* ten = new Op(10);
-        Op* six = new Op(6);
-        Sub* TreeC = new Sub(ten, six);
-
-	ListContainer* container = new ListContainer();
+TEST(FloorTest, num2){
+	Op* three = new Op(3.0);
+	Op* four = new Op(4.0);
+	Add* TreeB = new Add(three,four);
 	
-	container->add_element(TreeA);
-        container->add_element(TreeB);
-        container->add_element(TreeC);
+	Floor * floor2 = new Floor(TreeB);
+	ASSERT_EQ(floor2->evaluate(), 7.0);
+}
+
+TEST(num, truncate){
+	Op* four = new Op(4);
+
+	Op* five = new Op(5.0);
+	Mult* TreeA = new Mult(four, five);
 	
-	container->set_sort_function(new SelectionSort());
-	container->sort();
-
-	ASSERT_EQ(container->size(), 3);
-	EXPECT_EQ(container->at(0)->evaluate(), 4);
-	EXPECT_EQ(container->at(1)->evaluate(), 5);
-	EXPECT_EQ(container->at(2)->evaluate(), 28);	
-}
-TEST(StrategyTest, SelectionSortNegativeList) {
+	Trunc* trunk = new Trunc(TreeA);
+	ASSERT_EQ(trunk->stringify(),"20.000000");
+} 
+	
+TEST (num, paren){
+	Op* six = new Op(6);
 	Op* seven = new Op(7);
-        Op* negOne = new Op(-1);
-        Mult* TreeA = new Mult(seven, negOne);
-
-        Op* negThree = new Op(-3);
-        Op* negFive = new Op(-5);
-        Add* TreeB = new Add(negThree, negFive);
-
-        Op* six = new Op(6);
-        Op* eight = new Op(8);
-        Sub* TreeC = new Sub(six, eight);
-
-	ListContainer* container = new ListContainer();
-
-        container->add_element(TreeA);
-        container->add_element(TreeB);
-        container->add_element(TreeC);
-
-        container->set_sort_function(new SelectionSort());
-        container->sort();
-
-        ASSERT_EQ(container->size(), 3);
-        EXPECT_EQ(container->at(0)->evaluate(), -8);
-        EXPECT_EQ(container->at(1)->evaluate(), -7);
-        EXPECT_EQ(container->at(2)->evaluate(), -2);
+	Add* total = new Add(six, seven);
+	
+	Paren* result = new  Paren(total);
+	ASSERT_EQ(result->stringify(), "(6.000000 + 7.000000)");
 }
 
-TEST(Strategy, SelectionSortNegativeVector) {
-	Op* seven = new Op(7);
-        Op* negOne = new Op(-1);
-        Mult* TreeA = new Mult(seven, negOne);
-
-        Op* negThree = new Op(-3);
-        Op* negFive = new Op(-5);
-        Add* TreeB = new Add(negThree, negFive);
-
-        Op* six = new Op(6);
-        Op* eight = new Op(8);
-        Sub* TreeC = new Sub(six, eight);
-
-        VectorContainer* container = new VectorContainer();
-
-        container->add_element(TreeA);
-        container->add_element(TreeB);
-        container->add_element(TreeC);
-
-        container->set_sort_function(new SelectionSort());
-        container->sort();
-
-        ASSERT_EQ(container->size(), 3);
-        EXPECT_EQ(container->at(0)->evaluate(), -8);
-        EXPECT_EQ(container->at(1)->evaluate(), -7);
-        EXPECT_EQ(container->at(2)->evaluate(), -2);
+TEST(Combine, TruncCeil){
+	Op* num1 = new Op(20);
+	Op* num2 = new Op(10.4);
+	Add* TreeA = new Add(num1, num2);
+	
+	Ceil* ceil2 = new Ceil(TreeA);
+	Trunc* trunk = new Trunc(ceil2);
+	ASSERT_EQ(ceil2->evaluate(), 31.0);
+	ASSERT_EQ(trunk->stringify(), "31.000000");
 }
-TEST(Strategy, SelectionSortVector) {
-        Op* seven = new Op(7);
-        Op* One = new Op(1);
-        Mult* TreeA = new Mult(seven, One);
 
-        Op* Three = new Op(3);
-        Op* Five = new Op(5);
-        Add* TreeB = new Add(Three, Five);
+TEST(combine, parenAbs){
+	Op* num1 = new Op(32);
+	Op* num2 = new Op(12);
+	Sub* TreeA = new Sub(num2, num1);
+	
+	Abs* ab = new Abs(TreeA);
+	Paren* p = new Paren(ab);
+	ASSERT_EQ(p->stringify(), "(12.000000 - 32.000000)");
+} 
 
-        Op* three = new Op(3);
-        Op* two = new Op(2);
-        Sub* TreeC = new Sub(three, two);
+TEST(combine, TrAbsCeil){
+	Op* num2 = new Op(2);
+	Op* num1 = new Op(-9.3);
+	Mult* treeA = new Mult(num2, num1);
+	
+	Abs* ab = new Abs(treeA);
+	Ceil* c = new Ceil(ab);
+	Trunc* t = new Trunc(c);
 
-        VectorContainer* container = new VectorContainer();
-
-        container->add_element(TreeA);
-        container->add_element(TreeB);
-        container->add_element(TreeC);
-
-        container->set_sort_function(new SelectionSort());
-        container->sort();
-
-        ASSERT_EQ(container->size(), 3);
-        EXPECT_EQ(container->at(0)->evaluate(), 1);
-        EXPECT_EQ(container->at(1)->evaluate(), 7);
-        EXPECT_EQ(container->at(2)->evaluate(), 8);
+	ASSERT_EQ(t->stringify(), "19.000000");
 }
-TEST(StrategyTest, EmptyVector) {
-        VectorContainer* container = new VectorContainer();
-        ASSERT_EQ(container->size(), 0);
-        EXPECT_EQ(container->at(0), nullptr);
+	
+TEST(combine, ParenFloor){
+	Op* num = new Op(1.0);
+	Op* num2 = new Op(3.2);
+	Add* a = new Add(num, num2);
+	
+	Floor* f = new Floor(a);
+	Op* num3 = new Op(2.0);
+	Mult* m = new Mult(f,num3);
+	Paren* p = new Paren(m);
+	ASSERT_EQ(p->stringify(), "(4.000000 * 2.000000)");
+}	
+
+TEST(combine, ParenCeilAbs){
+	Op* num1 = new Op(3.9);
+	Op* num2 = new Op(2.0);
+	Sub* s = new Sub(num1, num2);
+	
+	Ceil* c = new Ceil(s);
+	Op* num3 = new Op(8);
+	Mult* m = new Mult(num3, c);
+
+	Paren* p = new Paren(m);
+	Op* num4 = new Op(-2);
+	Abs* a = new Abs(num4);
+	Add* a2 = new Add(p, a);
+	Paren* p2 = new Paren(a2);
+	ASSERT_EQ(p2->stringify(),"(16.000000 + 2.000000)");
 }
-TEST(StrategyTest, BubbleVector) {
-	Op* seven = new Op(7);
-        Op* four = new Op(4);
-        Mult* TreeA = new Mult(seven, four);
 
-        Op* three = new Op(3);
-        Op* two = new Op(2);
-        Add* TreeB = new Add(three, two);
-
-        Op* ten = new Op(10);
-        Op* six = new Op(6);
-        Sub* TreeC = new Sub(ten, six);
-
-        VectorContainer* container = new VectorContainer();
-        container->add_element(TreeA);
-        container->add_element(TreeB);
-        container->add_element(TreeC);
-
-        container->set_sort_function(new SelectionSort());
-        container->sort();
-
-        ASSERT_EQ(container->size(), 3);
-        EXPECT_EQ(container->at(0)->evaluate(), 4);
-        EXPECT_EQ(container->at(1)->evaluate(), 5);
-        EXPECT_EQ(container->at(2)->evaluate(), 28);
-
+TEST(combine, trFloor){
+	Op* num1 = new Op(1.2);
+	Op* num2 = new Op(1.3);
+	Add* a = new Add(num1, num2);
+	Floor* f = new Floor(a);
+	Op* num3 = new Op(4);
+	Mult* m = new Mult(num3, f);
+	Trunc* t = new Trunc(m);
+	ASSERT_EQ(t->stringify(),"8.000000");
 }
-TEST(StrategyTest, NegativeBubbleVector) { 
-	Op* seven = new Op(7);
-        Op* negOne = new Op(-1);
-        Mult* TreeA = new Mult(seven, negOne);
-
-        Op* negThree = new Op(-3);
-        Op* negFive = new Op(-5);
-        Add* TreeB = new Add(negThree, negFive);
-
-        Op* six = new Op(6);
-        Op* eight = new Op(8);
-        Sub* TreeC = new Sub(six, eight);
-
-        VectorContainer* container = new VectorContainer();
-
-        container->add_element(TreeA);
-        container->add_element(TreeB);
-        container->add_element(TreeC);
-
-        container->set_sort_function(new Bubble());
-        container->sort();
-
-        ASSERT_EQ(container->size(), 3);
-        EXPECT_EQ(container->at(0)->evaluate(), -8);
-        EXPECT_EQ(container->at(1)->evaluate(), -7);
-        EXPECT_EQ(container->at(2)->evaluate(), -2);
-}
-TEST(StrategyTest, PopulateVector) {
-	Op* seven = new Op(7);
-        Op* negOne = new Op(-1);
-        Mult* TreeA = new Mult(seven, negOne);
-
-        Op* negThree = new Op(-3);
-        Op* negFive = new Op(-5);
-        Add* TreeB = new Add(negThree, negFive);
-
-        Op* six = new Op(6);
-        Op* eight = new Op(8);
-        Sub* TreeC = new Sub(six, eight);
-
-        VectorContainer* container = new VectorContainer();
-
-        container->add_element(TreeA);
-        container->add_element(TreeB);
-        container->add_element(TreeC);
-
-        ASSERT_EQ(container->size(), 3);
-        EXPECT_EQ(container->at(0)->evaluate(), -7);
-        EXPECT_EQ(container->at(1)->evaluate(), -8);
-        EXPECT_EQ(container->at(2)->evaluate(), -2);
-}
+	
 int main(int argc, char**argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
